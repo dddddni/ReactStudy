57314063f736c535648ab06ecf241b1f40c7b912
dddddni <tjgus9812@gmail.com>
2020-12-15 16:12:11 +0900

    mobx로 counter 생성

---
+++ counter/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component} from 'react';
+import { decorate, observable, action } from 'mobx';
+import { observer } from 'mobx-react';
+
+class Counter extends Component {
+  number = 0;
+  
+  increase = () => {
+    this.number++;
+  }
+  
+  decrease = () => {
+    this.number--;
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>{this.number}</h1>
+        <button onClick={this.increase}>+1</button>
+        <button onClick={this.decrease}>-1</button>
+      </div>
+    )
+  }
+}
+
+// 데코레이터 설정 부분
+decorate(Counter ,{
+  number: 'observable',
+  increase: 'action',
+  decrease: 'action'
+});
+
+export default observer(Counter);
