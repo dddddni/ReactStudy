a9ac646724e0ba3e7f22ed3b9eb2d00853c7a8d6
dddddni <tjgus9812@gmail.com>
2020-12-21 17:49:02 +0900

    commit

---
+++ React+Core/ReactCRUD/ReactCRUD/Models/DCandidate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactCRUD.Models
+{
+    public class DCandidate
+    {
+        // prop + tab 키 누르면 자동 생성
+        public int MyProperty { get; set; }
+
+    }
+}
---
+++ React+Core/ReactCRUD/ReactCRUD/Models/DonationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactCRUD.Models
+{
+    public class DonationDBContext
+    {
+    }
+}
